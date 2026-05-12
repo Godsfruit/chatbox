92c399acd36a7ba11668f5a91f0717d1b798fccb
Godsfruit <godsfruitbookings@gmail.com>
2026-05-12 10:27:43 +0100

    Add initial HTML structure for index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GF ChatBoX | Welcome</title>
+    <link rel="stylesheet" href="assets/auth.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to GF_ChAtBoX</h1>
+        <div class="actions">
+            <a href="login.php"><button>Login</button></a>
+            <p></p>
+            <a href="register.php"><button>Register</button></a>
+        </div>
+    </div>
+</body>
+</html>
